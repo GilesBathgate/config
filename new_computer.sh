1e11e577e356bebe550cea9334ef896e0a93e69e
Giles Bathgate <gilesbathgate@gmail.com>
2014-04-19 13:24:06 +0100

    Changes needed for Ubuntu Trusty Tahr 14.04

--- new_computer.sh
+++ new_computer.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-
+apt-add-repository -y ppa:keepassx/daily
 apt-get -y update 
 apt-get -y dist-upgrade
-apt-get -y install gnome-session-fallback
-#logout
 
 #Stuff I dont want
 apt-get -y purge nvidia-common
+apt-get -y purge zeitgeist
 apt-get -y purge .*unity.*
-apt-get -y install unity-greeter
+apt-get -y install unity-greeter gnome-session-fallback
 apt-get -y install brasero brasero-cdrkit nautilus nautilus-sendto nautilus-share rhythmbox-plugin-cdrecorder shotwell 
 apt-get -y purge .*ubuntuone.*
 apt-get -y purge overlay-scrollbar
-apt-get -y purge gnome-orca
-apt-get -y purge onboard
+apt-get -y purge gnome-orca onboard
+apt-get -y purge firefox
+apt-get -y purge indicator-session
+apt-get -y purge appmenu-qt5
 
 #Stuff I need
-apt-get -y install git-core keepassx pidgin chromium-browser vim ntp virtualbox meld gitg
+apt-get -y install git keepassx pidgin chromium-browser vim ntp virtualbox meld gitg
 
 #Rapcad dependencies
 #apt-get -y install flex bison libcgal-dev libqt4-dev libdxflib-dev asciidoc source-highlight
